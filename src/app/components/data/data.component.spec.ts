97b0cd5e6f0adb6a1847dfe01887901b3afa47bf
dmitry.pavlov <rak.v.barake@gmail.com>
2020-06-05 16:43:27 +0400

    fix tests

--- src/app/components/data/data.component.spec.ts
+++ src/app/components/data/data.component.spec.ts
@@ -1,19 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { DataComponent } from './data.component';
 
 describe('DataComponent', () => {
   let component: DataComponent;
   let fixture: ComponentFixture<DataComponent>;
+  let store: MockStore;
+  const initialState = { AppState: { title: 'title' } };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DataComponent ]
+      declarations: [ DataComponent ],
+      providers: [
+        provideMockStore({ initialState }),
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(DataComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/components/title/title.component.spec.ts
+++ src/app/components/title/title.component.spec.ts
@@ -1,19 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { TitleComponent } from './title.component';
 
 describe('TitleComponent', () => {
   let component: TitleComponent;
   let fixture: ComponentFixture<TitleComponent>;
+  let store: MockStore;
+  const initialState = { AppState: { title: 'title' } };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TitleComponent ]
+      declarations: [ TitleComponent ],
+      providers: [
+        provideMockStore({ initialState }),
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(TitleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
